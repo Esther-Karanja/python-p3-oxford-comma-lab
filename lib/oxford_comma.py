f060e49a157be53f676915b8cf782a35c858cebb
Esther Karanja <est.karanja@gmail.com>
2023-10-13 13:11:40 -0700

    completed lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,11 @@
 def oxford_comma(items):
-    return None
+  if len(items) ==1:
+    return f"{items[0]}"
+  elif len(items) ==2:
+    return f"{items[0]} and {items[1]}"
+  else:
+    comma_separator = ", ".join(items[:-1])
+    return f"{comma_separator}, and {items[-1]}"
+
+
+
